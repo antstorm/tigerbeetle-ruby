68fe0ff54739a06e1cb892f4c6be29a82d2cf0e7
antstorm <antstorm@gmail.com>
2025-12-06 15:38:08 +0000

    Update tb_client to 0.16.64

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.28
+
+- Based on TigerBeetle 0.16.64
+
 ## 0.0.27
 
 - Based on TigerBeetle 0.16.63
--- lib/tigerbeetle/version.rb
+++ lib/tigerbeetle/version.rb
@@ -1,4 +1,4 @@
 module TigerBeetle
-  VERSION = '0.0.27'.freeze
-  TB_VERSION = '0.16.63'.freeze
+  VERSION = '0.0.28'.freeze
+  TB_VERSION = '0.16.64'.freeze
 end
